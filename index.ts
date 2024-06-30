4f226a586f30cff6f94eba4cfd082dd73431d889
Armand <64407468+Armandirow@users.noreply.github.com>
2024-06-30 19:33:18 +0200

    Update index.ts

--- index.ts
+++ index.ts
@@ -2,6 +2,7 @@ import { Client } from "discord.js";
 import { config } from "./config";
 import { commands } from "./commands";
 import { deployCommands } from "./deploy-commands";
+import { ButtonType } from "./types/counter.types";
 
 const client = new Client({
   intents: ["Guilds", "GuildMessages", "DirectMessages"],
@@ -29,11 +30,21 @@ client.on("interactionCreate", async (interaction) => {
       }
     } else if (interaction.isButton()) {
       // respond to the button
-
       const [counterName, counterString] =
         interaction.message.content.split(": ");
       const counter = Number(counterString);
-      interaction.update(`${counterName}: ${counter + 1}`);
+      switch (interaction.customId) {
+        case ButtonType.PLUS_ONE:
+          await interaction.update(`${counterName}: ${counter + 1}`);
+          break;
+
+        case ButtonType.MINUS_ONE:
+          await interaction.update(`${counterName}: ${counter - 1}`);
+          break;
+        default:
+          console.log("Button not connu en fait");
+          break;
+      }
     }
   } catch (error) {
     console.warn(`${error}`);
